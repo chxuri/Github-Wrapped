1825df71df182abb63b30c72b717e2279d4f5639
chxuri <rmamedova2010@gmail.com>
2026-01-14 18:33:58 -0600

    working on credentials for github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,34 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to OAUTH_CLIENT.auth_code.authorize_url(
+      redirect_uri: callback_url,
+      scope: "user:email read:user"
+    ), allow_other_host: true
   end
 
   def create
+    code = params[:code]
+    token = OAUTH_CLIENT.auth_code.get_token(code, redirect_uri: callback_url)
+    response = token.get("https://api.github.com/user")
+    user_data = JSON.parse(response.body)
+
+    user = User.find_or_create_by(email: user_data["email"]) do |u|
+      u.name = user_data["name"]
+    end
+
+    session[:user_id] = user.id
+
+    redirect_to root_path, notice: "Signed in as #{user.name}"
   end
+
+  def callback_url
+    "#{request.base_url}/auth/callback"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out!"
+  end
+
+  # token.get("https://api.github.com/user/repos")
 end
