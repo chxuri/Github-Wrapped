2f97956540bf47344f22dbf1db17cea3d26c48f3
chxuri <rmamedova2010@gmail.com>
2026-02-15 18:57:10 -0600

    fixed oauth + cleaned logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts callback_url
+    # puts callback_url
     code = params[:code]
     token = OAUTH_CLIENT.auth_code.get_token(code, redirect_uri: callback_url)
     response = token.get("https://api.github.com/user")
@@ -27,7 +27,12 @@ class SessionsController < ApplicationController
   # end
 
   def callback_url
-    "#{request.base_url}/auth/callback"
+    if Rails.env.production?
+      "https://brief-kaycee-chxuri-cac0f3b7.koyeb.app/auth/callback"
+    else
+      "http://localhost:3000/auth/callback"
+    end
+    # "#{request.base_url}/auth/callback"
   end
 
   def destroy
@@ -35,17 +40,10 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: "Logged out!"
   end
 
-  def callback_url
-    if Rails.env.production?
-      "https://brief-kaycee-chxuri-cac0f3b7.koyeb.app/auth/callback"
-    else
-      "#{request.base_url}/auth/callback"
-    end
-  end
-
 
   def index
     return unless logged_in?
+    # Rails.logger.debug "TOKEN: #{session[:github_token].inspect}"
     response = Faraday.get("https://api.github.com/user/repos") do |req|
       req.headers["Authorization"] = "Bearer #{session[:github_token]}"
       req.headers["Accept"] = "application/vnd.github.v3+json"
@@ -55,20 +53,26 @@ class SessionsController < ApplicationController
     # puts "Response status: #{response.status}"
     # puts "Response body: #{response.body.inspect}"
     @repos = JSON.parse(response.body)
-
-    test = Faraday.get("https://api.github.com/repos/#{@repos[1]["owner"]["login"]}/#{@repos[1]["name"]}/commits") do |req|
-          req.params["page"] = 1
-          req.params["per_page"] = 100
-          req.headers["Authorization"] = "Bearer #{session[:github_token]}"
-          req.headers["Accept"] = "application/vnd.github.v3+json"
-          req.headers["User-Agent"] = "wrapped"
+    unless @repos.is_a?(Array)
+      @repos = []
+      return
     end
 
-    @onePageTest = JSON.parse(test.body)
+    return if @repos.blank?
 
-    maxCommits = 0
-    @mostCommitted = @repos[0]
-    @longestTimeRepo = @repos[0]
+    # test = Faraday.get("https://api.github.com/repos/#{@repos[1]["owner"]["login"]}/#{@repos[1]["name"]}/commits") do |req|
+    #      req.params["page"] = 1
+    #      req.params["per_page"] = 100
+    #      req.headers["Authorization"] = "Bearer #{session[:github_token]}"
+    #   req.headers["Accept"] = "application/vnd.github.v3+json"
+    #  req.headers["User-Agent"] = "wrapped"
+    # end
+
+    # @onePageTest = JSON.parse(test.body)
+
+    @maxCommits = 0
+    @mostCommitted = @repos.first
+    @longestTimeRepo = @repos.first
     @commits = ""
     @mostTime = 0
 
@@ -77,6 +81,8 @@ class SessionsController < ApplicationController
       commitCount = 0
       page = 1
       loop do
+        # Rails.logger.info repo.class
+        # Rails.logger.info repo.inspect
         commitList = Faraday.get("https://api.github.com/repos/#{repo["owner"]["login"]}/#{repo["name"]}/commits") do |req|
           req.params["page"] = page
           req.params["per_page"] = 100
@@ -88,55 +94,59 @@ class SessionsController < ApplicationController
         @commits = JSON.parse(commitList.body)
 
         break if @commits.empty?
-        commitCount += @commits.size
-        page += 1
+        # commitCount += @commits.size
+        # page += 1
 
         # loops thru commits in each repo and checks date + time
         @loopCounter = 0
         for i in 0..@commits.length-2
-          # if @commits[i]["commit"]["author"]["login"] == ""
-          # <!--<p>name: <%= @current_user.name %><p>-->
-          @commitCheck = @commits[0]["commit"]["author"]["date"][0, 4].to_i
-          @debugger = "works"
-          year2025 = (@commits[i]["commit"]["author"]["date"][0, 4].to_i == 2025)
-          month = @commits[i]["commit"]["author"]["date"][5, 7].to_i
-          nextMonth = @commits[i+1]["commit"]["author"]["date"][5, 7].to_i
-          day = @commits[i]["commit"]["author"]["date"][8, 10].to_i
-          nextDay = @commits[i+1]["commit"]["author"]["date"][8, 10].to_i
-          hour = @commits[i]["commit"]["author"]["date"][11, 13].to_i
-          nextHour = @commits[i+1]["commit"]["author"]["date"][11, 13].to_i
-          minute = @commits[i]["commit"]["author"]["date"][14, 16].to_i
-          nextMinute = @commits[i+1]["commit"]["author"]["date"][14, 16].to_i
+          if @commits[i]["author"] && @commits[i]["author"]["login"] == session[:github_username]
 
-          checkTime = false
+            # <!--<p>name: <%= @current_user.name %><p>-->
+            # @commitCheck = @commits[0]["commit"]["author"]["date"][0, 4].to_i
+            # @debugger = "works"
+            year2025 = (@commits[i]["commit"]["author"]["date"][0, 4].to_i == 2025)
+            month = @commits[i]["commit"]["author"]["date"][5, 7].to_i
+            nextMonth = @commits[i+1]["commit"]["author"]["date"][5, 7].to_i
+            day = @commits[i]["commit"]["author"]["date"][8, 10].to_i
+            nextDay = @commits[i+1]["commit"]["author"]["date"][8, 10].to_i
+            hour = @commits[i]["commit"]["author"]["date"][11, 13].to_i
+            nextHour = @commits[i+1]["commit"]["author"]["date"][11, 13].to_i
+            minute = @commits[i]["commit"]["author"]["date"][14, 16].to_i
+            nextMinute = @commits[i+1]["commit"]["author"]["date"][14, 16].to_i
 
-          if year2025 && (month == nextMonth) && (day == nextDay)
-            if (nextHour - hour <= 1) || (nextHour - hour == 2 && (60 - minute + nextMinute + 60 <= 90))
-              checkTime = true
+            checkTime = false
+
+            if year2025 && (month == nextMonth) && (day == nextDay)
+              if (nextHour - hour <= 1) || (nextHour - hour == 2 && (60 - minute + nextMinute + 60 <= 90))
+                checkTime = true
+              end
+            elsif year2025 && (month == nextMonth + 1 && nextDay == 1) || (day + 1 == nextDay)
+              if (nextHour - hour <= 1) || ((nextHour - hour == 2 || 12 - nextHour - hour == 2) && (60 - minute + nextMinute + 60 <= 90))
+                checkTime = true
+              end
             end
-          elsif year2025 && (month == nextMonth + 1 && nextDay == 1) || (day + 1 == nextDay)
-            if (nextHour - hour <= 1) || ((nextHour - hour == 2 || 12 - nextHour - hour == 2) && (60 - minute + nextMinute + 60 <= 90))
-              checkTime = true
+
+            if checkTime
+              if nextHour - hour == 0
+                repoTime += nextMinute - minute
+              elsif nextHour - hour == 1
+                repoTime += 60 - minute + nextMinute
+              else
+                repoTime += 60 - minute + nextMinute + 60
+              end
             end
+
+            @loopCounter += 1
+            commitCount += 1
           end
-
-          if checkTime
-            if nextHour - hour == 0
-              repoTime += nextMinute - minute
-            elsif nextHour - hour == 1
-              repoTime += 60 - minute + nextMinute
-            else
-              repoTime += 60 - minute + nextMinute + 60
-            end
-          end
-
-          @loopCounter += 1
         end
+        page += 1
       end
 
-      if commitCount > maxCommits
+      if commitCount > @maxCommits
         @mostCommitted = repo
-        maxCommits = commitCount
+        @maxCommits = commitCount
       end
 
       if repoTime > @mostTime
--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,8 +1,8 @@
 require "oauth2"
 
 OAUTH_CLIENT = OAuth2::Client.new(
-  ENV["OAUTH_CLIENT_ID"],
-  ENV["OAUTH_CLIENT_SECRET"],
+  ENV.fetch("OAUTH_CLIENT_ID"),
+  ENV.fetch("OAUTH_CLIENT_SECRET"),
   site: "https://github.com",
   authorize_url: "/login/oauth/authorize",
   token_url: "/login/oauth/access_token"
